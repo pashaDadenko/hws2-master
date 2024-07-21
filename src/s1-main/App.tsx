79fe978544b8742b2a3e88e5c02d8aa0ec923d78
pashaDadenko <pasha061090@mail.ru>
2024-07-21 13:02:15 +0300

    1

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,4 +1,4 @@
-import HW6 from '../s2-homeworks/hw06/HW6';
+import HW5 from '../s2-homeworks/hw05/HW5';
 import s from './App.module.css';
 
 function App() {
@@ -6,12 +6,12 @@ function App() {
 		<div className={s.App}>
 			{/* <HW1 /> */}
 			{/*раскомментировать по ходу выполнения*/}
-			{/*<HW2/>*/}
-			{/*<HW3/>*/}
-			{/*<HW4/>*/}
+			{/* <HW2/>
+			<HW3/>
+			<HW4 /> */}
 
 			{/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-			<HW6 />
+			<HW5 />
 		</div>
 	);
 }
