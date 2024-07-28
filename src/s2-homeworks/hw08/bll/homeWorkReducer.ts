0d96f9f9362e4de56c202d425f653bfa1a7ad78f
pashaDadenko <pasha061090@mail.ru>
2024-07-28 13:45:29 +0300

    1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,20 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+	switch (action.type) {
+		case 'sort': {
+			const sortedState = [...state].sort((a, b) => {
+				if (action.payload === 'up') return a.name.localeCompare(b.name);
+				return b.name.localeCompare(a.name);
+			});
+			return sortedState;
+		}
+		case 'check': {
+			return state.filter((user) => user.age >= action.payload);
+		}
+		default:
+			return state;
+	}
+};
