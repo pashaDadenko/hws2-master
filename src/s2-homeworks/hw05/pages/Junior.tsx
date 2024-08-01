2caa76442077985b31fced6a7fee2d211da1cc7e
pashaDadenko <pasha061090@mail.ru>
2024-08-01 22:56:47 +0300

    1

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,12 +1,12 @@
-import HW8 from '../../hw08/HW8';
+import HW9 from '../../hw09/HW9';
 
 function Junior() {
 	return (
 		<div id={'hw5-page-junior'}>
 			{/* <HW6 /> */}
 			{/* <HW7 /> */}
-			<HW8 />
-			{/*<HW9 />*/}
+			{/* <HW8 /> */}
+			<HW9 />
 		</div>
 	);
 }
